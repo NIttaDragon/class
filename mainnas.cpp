1b02335f179f626441422bbb86440e14a7628d16
NittaDragon <uspemskaya@gmail.com>
2022-05-16 12:56:35 +0300

    mbfinal

--- mainnas.cpp
+++ mainnas.cpp
@@ -14,17 +14,9 @@ int main()
   CPolygon Q; //объявление объекта класса
   CPolygon *Q2=new CPolygon; //массив указателей
   int c,k,b,x,y,z,n;
-  // M2->NachMatr();
-  // M2->show();
-  cout<<"Для Многоугольника (семиугольника)"<<endl;
-  n=7;
-  CPolygon P1(n);
-  P2->Perimetr(n);
-  cout<<"Для Треугольника"<<endl;
   n=3;
   CTriangle T1(n);
   T2->Perimetr(n);
-  cout<<"Для Четырёхугольника"<<endl;
   n=4;
   CQuad Q1(n);
   Q2->Perimetr(n);
@@ -33,7 +25,6 @@ int main()
   {
     cout<<endl;
     cout<<"Что хотите сделать с объектами:"<<endl;
-    //cout<<"0. Заполнить значения вершин объектов"<<endl;
     cout<<"1. Вывести состояние Треугольника"<<endl;
     cout<<"2. Вывести состояние Квадрата"<<endl;
     cout<<"3. Вызовов виртуальной функции для Тругольника или Кавдрата"<<endl;
@@ -44,22 +35,6 @@ int main()
     cout<<endl;
     switch (b)
     {
-      // case 0:
-      // {
-      //   cout<<"Для Многоугольника (семиугольника)"<<endl;
-      //   n=7;
-      //   CPolygon P1(n);
-      //   P2->Perimetr(n);
-      //   cout<<"Для Треугольника"<<endl;
-      //   n=3;
-      //   CTriangle T1(n);
-      //   T2->Perimetr(n);
-      //   cout<<"Для Четырёхугольника"<<endl;
-      //   n=4;
-      //   CQuad Q1(n);
-      //   Q2->Perimetr(n);
-      //   break;
-      // }
       case 1:
       {
         n=3;
--- specnasnew.h
+++ specnasnew.h
@@ -8,25 +8,21 @@ protected:
   int d[7];
   int n=7;
 public:
-  int per;
-  CPolygon(){} //конструктор класса
-  CPolygon(int n) //конструктор с параметром
+  int per=0;
+  CPolygon() //конструктор класса
   {
-    cout<<"Введите значения вершин: "<<endl;
     for(int i=0;i<n;i++)
       for(int j=0;j<2;j++)
-      {
-        if(j==0)
-          cout<<" x= ";
-        else
-          cout<<" y= ";
-        cin>>vp[i][j];
-      }
-    cout<<endl;
+        vp[i][j]=3*i+2*j;
+  }
+  CPolygon(int n) //конструктор с параметром
+  {
+    for(int i=0;i<n;i++)
+      for(int j=0;j<2;j++)
+        vp[i][j]=3*i+2*j;
   }
   virtual void Perimetr(int n) //вычисление периметра
   {
-    int per=0;
     for(int i=0;i<(n-1);i++)
       for(int j=0;j<1;j++)
         d[i]=sqrt(pow((vp[i][j]-vp[i+1][j]),2)+pow((vp[i][j+1]-vp[i+1][j+1]),2));
@@ -43,7 +39,7 @@ public:
         if(j==0)
           cout<<"x= "<<vp[i][j]<<"   ";
         else
-          cout<<"y= "<<endl;
+          cout<<"y= "<<vp[i][j]<<endl;
   }
   ~CPolygon() //деструктор
   {
@@ -60,27 +56,6 @@ public:
   int per;
   CTriangle():CPolygon(){}; //конструктор класса
   CTriangle(int n):CPolygon(n){}; //конструктор с параметром
-  void Perimetr(int n) override
-  {
-    int per=0;
-    for(int i=0;i<(n-1);i++)
-      for(int j=0;j<1;j++)
-        d[i]=sqrt(pow((vt[i][j]-vt[i+1][j]),2)+pow((vt[i][j+1]-vt[i+1][j+1]),2));
-    d[n]=sqrt(pow((vt[n][0]-vt[0][0]),2)+pow((vt[n][1]-vt[0][1]),2));
-    for(int i=0;i<n;i++)
-      per=per+d[i];
-  }
-  void show(int n) //override //вывод матрицы матрицы
-  {
-    cout<<"Периметр= "<<per<<endl;
-    cout<<"Вершины: "<<endl;
-    for(int i=0;i<n;i++)
-      for(int j=0;j<2;j++)
-        if(j==0)
-          cout<<"x= "<<vt[i][j]<<"   ";
-        else
-          cout<<"y= "<<vt[i][j]<<endl;
-  }
   ~CTriangle() //деструктор
   {
     cout<<"Треугольник удален"<<endl;
@@ -96,27 +71,6 @@ public:
   int per;
   CQuad():CPolygon(){}; //конструктор класса
   CQuad(int n):CPolygon(n){}; //конструктор с параметром
-  void Perimetr(int n) override
-  {
-    int per=0;
-    for(int i=0;i<(n-1);i++)
-      for(int j=0;j<1;j++)
-        d[i]=sqrt(pow((vq[i][j]-vq[i+1][j]),2)+pow((vq[i][j+1]-vq[i+1][j+1]),2));
-    d[n]=sqrt(pow((vq[n][0]-vq[0][0]),2)+pow((vq[n][1]-vq[0][1]),2));
-    for(int i=0;i<n;i++)
-      per=per+d[i];
-  }
-  void show(int n) //override //вывод матрицы матрицы
-  {
-    cout<<"Периметр= "<<per<<endl;
-    cout<<"Вершины: "<<endl;
-    for(int i=0;i<n;i++)
-      for(int j=0;j<2;j++)
-        if(j==0)
-          cout<<"x= "<<vq[i][j]<<"   ";
-        else
-          cout<<"y= "<<vq[i][j]<<endl;
-  }
   ~CQuad() //деструктор
   {
     cout<<"Квадрат удален"<<endl;
